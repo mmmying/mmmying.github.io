043f05c913852edefbe026b5abfb8cb93726f644
mmmying <987042638@qq.com>
2025-07-08 11:37:50 +0800

    Site updated: 2025-07-08 11:37:44

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2021/03/28/p001/","2021/04/08/p002/","2021/04/22/p003/","2021/05/19/p004/","2021/06/01/p005/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
